0e65cb300d0025f4a359a33c41f781300c11cf92
jonathgomz <jonathgomz2003@gmail.com>
2025-09-15 19:57:34 -0500

    Problema 1 lab 3 Listo

---
+++ Laboratorio 2/Problema 1 lab 2/Problema 1 lab 2/Problema 1 lab 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P1Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.FirstName = "Jonathan";
+            client.LastName = "Gomez";
+            client.Age = 22;
+            client.Id = 8 - 1002 - 785;
+
+            Console.WriteLine(client.GetFullName());
+        }
+        public class Client
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+            public int Id { get; set; }
+            public string GetFullName()
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+    }
+}
