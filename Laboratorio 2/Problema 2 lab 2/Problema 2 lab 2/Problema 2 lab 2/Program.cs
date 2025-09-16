41e48d085f1eb4f424e5c128ba36ea24be288689
jonathgomz <jonathgomz2003@gmail.com>
2025-09-15 19:51:37 -0500

    Problema 2 lab 2 compelto

---
+++ Laboratorio 2/Problema 2 lab 2/Problema 2 lab 2/Problema 2 lab 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Labortorio21
+{
+    public class program
+    {
+    public static void Main()
+        {
+        MyClass.Valor = 1;
+        Console.WriteLine(MyClass.Valor); 
+        }
+    }
+public class MyClass
+    {
+        public static int Valor;
+}
+}
