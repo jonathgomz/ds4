505310ae790699f65f4f5fd8192e8c737f5975ae
jonathgomz <jonathgomz2003@gmail.com>
2025-09-19 14:07:52 -0500

    Laboratorio  4 listo

---
+++ Laboratorio4/Laboratorio 44/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[]args)
+    {
+        Console.WriteLine("ingrese lal nota del estudiante");
+        float score = float.Parse(Console.ReadLine());
+    if (score >= 70)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"su nota es {score} ha aprobado");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine($"su nota es {score} ha reprobado, debe repetir");
+        }
+    }
+}
