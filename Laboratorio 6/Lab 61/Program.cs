7d5f65800688eabe0982fc7ea6e7c9f0ea7869b0
jonathgomz <jonathgomz2003@gmail.com>
2025-09-22 20:05:26 -0500

    Laboratorio 6 terminado

---
+++ Laboratorio 6/Lab 61/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main (String[]args)
+    {
+        int num;
+        Console.WriteLine("Digite el numero deseado");
+        try
+        {
+            num = Int16.Parse(Console.ReadLine());
+
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("No ha introducido un digito valido");
+            num = 1;
+        }
+        Console.WriteLine(num);
+    }
+}
---
+++ Laboratorio 6/Lab 63/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(String[] args)
+    { 
+        try 
+        {
+    int[] myNumbers = { 1, 2, 3 };
+    Console.WriteLine(myNumbers[10]);
+        }
+            catch (Exception e)
+            {
+    Console.WriteLine("algo salio mal, valide el indice del arreglo");
+            }
+    finally
+    {
+        Console.WriteLine("Continuacion de la apliacion");
+        }
+    }
+}
