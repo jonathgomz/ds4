1481e78adaab06a9e9f04564e6e5dc672d5ae2f1
jonathgomz <jonathgomz2003@gmail.com>
2025-09-24 18:39:13 -0500

    Lab 71 terminado

---
+++ Laboratorio 7/Lab 71/Lab 71/Program.cs
@@ -0,0 +1,11 @@
+﻿using Lab_71;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Banco banco1 = new Banco();
+        banco1.Operar();
+        banco1.DepositosTotales();
+        Console.ReadKey();
+    }
+}
