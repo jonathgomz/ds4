240c19f8444616ccd037a940baba69114d1fbaaf
Jonathan <Jonathan@DESKTOP-UEVN9UU>
2025-11-17 18:11:48 -0500

    Lab 17 terminado

---
+++ Laboratorio 17/Laboratorio 171/Laboratorio 171/WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Laboratorio_171
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ConnectionStringSettings connString = ConfigurationManager.ConnectionStrings["ConexionNorthWind"];
+            SqlConnection conexion = new SqlConnection(connString.ConnectionString);
+
+            using (SqlCommand cmd = new SqlCommand("SalesByCategory", conexion))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@CategoryName", SqlDbType.NVarChar).Value = "Seafood";
+                conexion.Open();
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        if (reader.Read())
+                        {
+                            GridV.DataSource = reader;
+                            GridV.DataBind();
+                        }
+                    }
+                }
+
+
+            }
+        }
+    }
+}
